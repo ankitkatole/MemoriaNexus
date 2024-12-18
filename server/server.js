10d7478dcd0cb4a9f398d28425fb8a4c1bc4d66e
JiteshVarade <jiteshvarade1@gmail.com>
2024-12-18 17:04:21 +0530

    Added socket.io setup

--- server/server.js
+++ server/server.js
@@ -1,20 +1,40 @@
 const express = require('express');
 const cors = require('cors');
+const {createServer} = require('http')
+const {Server} = require('socket.io')
 
 const {PORT} = require('./constants');
 const {ConnectDB} = require('./src/db/connection');
 const {userRouter} = require('./src/routes/user');
 
-const app = express();
+if (require.main === module) {
 
-app.use(express.json()); 
-app.use(cors());
+    // Database connection
+    ConnectDB();
+    
+    const app = express();
+    app.use(express.json()); 
 
-ConnectDB();
+    // cors
+    app.use(cors({origin : true}));
 
-app.use("/user",userRouter);
+    // socket.io server
+    const server = createServer(app)
+    const io = new Server(server, {
+        cors : {
+            origin : true
+        }
+    })
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-    console.log(`http://localhost:${PORT}`);
-});
+    io.on("connection",(socket)=>{
+        console.log("User connected : ",socket.id)
+    })
+
+    server.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+        console.log(`http://localhost:${PORT}`);
+    });
+
+    // Routes
+    app.use("/user",userRouter);
+}
