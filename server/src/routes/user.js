7eeee130e4bb044b932013615ff9d275e8a95b5c
ankitkatole <katoleankit06@gmail.com>
2024-12-20 21:30:49 +0530

    Signout functionality

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -1,10 +1,11 @@
-const {signup , signin} = require("../controllers/user");
+const {signup , signin,signout} = require("../controllers/user");
 const { Router } = require("express");
 const userRouter = Router();
 
 userRouter.post("/signup", signup);
 
 userRouter.post("/signin", signin)
+userRouter.post("/signout", signout)
 
 module.exports = {
     userRouter
