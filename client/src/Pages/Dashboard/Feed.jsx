109db7e35d108889c340827f1de43ca5a76a3a40
ankitkatole <katoleankit06@gmail.com>
2024-12-21 14:08:31 +0530

    updated feed.jsx

--- client/src/Pages/Dashboard/Feed.jsx
+++ client/src/Pages/Dashboard/Feed.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-import Joke from '../../assets/joke.svg'
+import Joke from '../../assets/Joke.svg'
 
 function Feed() {
   const [joke, setJoke] = useState('');
