a8279e222016bbfcf018d418494feca7163a441c
JiteshVarade <jiteshvarade1@gmail.com>
2024-12-21 17:20:18 +0530

    user can see all his forums

--- server/src/controllers/forum.js
+++ server/src/controllers/forum.js
@@ -24,4 +24,25 @@ const createForum = async (req, res) => {
   }
 };
 
-module.exports = { createForum };
+const getForumUsingUserId = async (req, res) => {
+    const { userId } = req.params;
+
+    if (!userId) {
+        return res.status(400).json({ message: 'User ID is required.' });
+    }
+
+    try {
+        const forums = await Forum.find({ members: userId });
+
+        if (!forums.length) {
+            return res.status(404).json({ message: 'No forums found for this user.' });
+        }
+
+        res.status(200).json(forums);
+    } catch (error) {
+        console.error('Error fetching forums:', error.message);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+};
+
+module.exports = { createForum, getForumUsingUserId };
--- server/src/routes/forum.js
+++ server/src/routes/forum.js
@@ -1,7 +1,10 @@
-const {createForum} = require("../controllers/forum");
+const {createForum, getForumUsingUserId} = require("../controllers/forum");
 const { Router } = require("express");
 const router = Router();
 
 router.post("/createForum", createForum);
+
+// returs all the forum in which user is present
+router.get("/getForumUsingUserId/:userId", getForumUsingUserId);
 
 module.exports = router;
