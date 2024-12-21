fb23c39976a35742d33f11663bae09493cc821aa
ankitkatole <katoleankit06@gmail.com>
2024-12-21 21:37:37 +0530

    added ChatNexus

--- client/src/constant.mjs
+++ client/src/constant.mjs
@@ -1,3 +1,4 @@
 const SERVER_URL = import.meta.env.VITE_WEB_SERVER_URL
-
+export const APPID = import.meta.env.APPID;
+export const SERVERSECRET = import.meta.env.SERVERSECRET;
 export default SERVER_URL
