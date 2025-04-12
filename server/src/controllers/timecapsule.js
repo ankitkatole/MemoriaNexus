38e0ddada9c9a7da62ebae39bbc1ebde827da743
gajju44 <gajjunaphade@gmail.com>
2025-04-12 21:15:37 +0530

    timecapsule

--- server/src/controllers/timecapsule.js
+++ server/src/controllers/timecapsule.js
@@ -48,7 +48,7 @@ const unlockTimeCapsule = async (req, res) => {
         }
 
         // Check if the user is authorized to unlock the Time Capsule
-        if (timeCapsule.user_id.toString() !== user._id.toString()) {
+        if (timeCapsule.user_id.toString() !== user.username) {
             return res.status(403).json({
                 message: "You are not authorized to unlock this time capsule"
             });
